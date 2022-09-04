ff879fd90395da57a3027b6318453e3d53cc0e9e
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-04 03:47:34 +0200

    Update path.py

--- config/qtile/settings/path.py
+++ config/qtile/settings/path.py
@@ -1,3 +1,8 @@
+# Pingu's Qtile Settings (path) Config File
+# (c) iWas. All rights reserved.
+# https://github.com/iWas-Coder/Pingu
+
 from os import path
+
 
 qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
