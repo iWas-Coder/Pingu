40e588494e5071020a49bb29894b3332e3bb7526
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-12 10:56:15 +0200

    Create widgets.py

---
+++ config/qtile/settings/widgets.py
@@ -0,0 +1,13 @@
+# Pingu's Qtile Settings (widgets) Config File
+# (c) iWas. All rights reserved.
+# https://github.com/iWas-Coder/Pingu
+
+from libqtile import widget
+
+
+widget_defaults = {
+    "font": "JetBrainsMono Nerd Font",
+    "fontsize": 11,
+    "padding": 3
+}
+extension_defaults = widget_defaults.copy()
