ddaeec65539860fb97a2015859ecbcb6e54f183e
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-19 21:02:48 +0200

    Delete config directory

--- config/qtile/settings/path.py
+++
@@ -1,9 +0,0 @@
-# Pingu's Qtile Settings (path) Config File
-# (c) iWas. All rights reserved.
-# https://github.com/iWas-Coder/Pingu
-
-from os import path
-
-
-# === Qtile Path Variable Export === #
-qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
