3bc5c54bea590f9c07c2deb6e4460f5a0befcf87
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-24 14:57:20 +0200

    Create path.py

---
+++ config/qtile/settings/path.py
@@ -0,0 +1,8 @@
+# === Qtile (PATH) Configuration by iWas <3 === #
+
+
+from os import path
+
+
+# === Qtile Path Variable Export === #
+qtile_path = path.join(path.expanduser('~'), ".config", "qtile")
