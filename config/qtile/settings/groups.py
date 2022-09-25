085aa1d7d07079f24b68056ec4f1fe6ccc084ccb
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-25 22:23:58 +0200

    Update groups.py

--- config/qtile/settings/groups.py
+++ config/qtile/settings/groups.py
@@ -3,7 +3,7 @@
 
 from libqtile.config import Key, Group
 from libqtile.command import lazy
-from .keys import mod, keys
+from .keys import ctrl, keys
 
 
 # === Groups list === #
@@ -13,7 +13,7 @@ for i, group in enumerate(groups):
     actual_key = str(i + 1)
     keys.extend([
         # Switch to workspace N
-        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        Key([ctrl], actual_key, lazy.group[group.name].toscreen()),
         # Send window to workspace N
-        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+        Key([ctrl, "shift"], actual_key, lazy.window.togroup(group.name))
     ])
