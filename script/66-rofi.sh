c1c3ce0963f30e79d90714a4b21d563503527319
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-10-07 21:20:20 +0200

    Delete 66-rofi.sh

--- script/66-rofi.sh
+++
@@ -1,7 +0,0 @@
-echo "[*] Configuring Rofi..."
-sleep 2
-cd $MAIN_PWD
-cp -r ./config/rofi ~/.config/rofi
-sudo mv ~/.config/rofi/onedark.rasi /usr/share/rofi/themes
-sleep 2
-echo "[+] Rofi Configured!"
