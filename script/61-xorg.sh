026a9359c36c77384236fd2052da36b5a39bb00d
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-10-07 21:04:10 +0200

    Delete 61-xorg.sh

--- script/61-xorg.sh
+++
@@ -1,9 +0,0 @@
-echo "[*] Configuring Xorg..."
-sleep 2
-cd $MAIN_PWD
-sudo cp ./config/xorg/xinitrc /etc/X11/xinit/xinitrc
-sudo cp ./config/xorg/xprofile ~/.xprofile
-sleep 2
-echo "[+] Xorg Configured!"
-
-sleep 2
